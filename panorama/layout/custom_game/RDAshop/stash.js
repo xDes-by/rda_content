d716e791f97b49c4dd05d86fe6902c292038cc63
dansoo0911 <dansoo0911@gmail.com>
2023-09-01 11:19:05 +0300

    123

--- panorama/layout/custom_game/RDAshop/stash.js
+++ panorama/layout/custom_game/RDAshop/stash.js
@@ -10,6 +10,18 @@ const ITEMS_TO_VIEW =
     "item_attack_speed_aura",
     "item_hp_aura",
 ]
+const BUFF_CD_MODIFIERS = 
+[
+    "",
+    "",
+    "",
+    "modifier_item_armor_aura_cd",
+    "modifier_item_base_damage_aura_cd",
+    "modifier_item_expiriance_aura_cd",
+    "modifier_item_move_aura_cd",
+    "modifier_item_attack_speed_aura_cd",
+    "modifier_item_hp_aura_cd",
+]
 
 function FindItemInShop(tab, itemname){
     for(let categoryKey in tab){
@@ -32,6 +44,8 @@ function InitStash(t){
         itemPanel.FindChildTraverse("ItemImage").itemname = itemname
         const ItemInfo = FindItemInShop(t, itemname)
         itemPanel.SetPanelEvent("onactivate", TakeItem(itemname))
+        // itemPanel.SetPanelEvent("oncontextmenu", TakeItem(itemname))
+        itemPanel.SetPanelEvent("onl", TakeItem(itemname))
         if(!ItemInfo){
             itemPanel.visible = false
         }else{
@@ -45,14 +59,50 @@ function TakeItem(itemname){
         const itemPanel = $(`#${itemname}_Panel`)
         const itemCountPanel = itemPanel.FindChildTraverse("ItemCount")
         const updatedCount = Number(itemCountPanel.text) - 1
+        const IsControlDown = GameUI.IsControlDown()
         if(updatedCount < 0) return
         itemCountPanel.text = updatedCount
         GameEvents.SendCustomGameEventToServer ("CustomShopStash_TakeItem",{
-            itemname : itemname
+            itemname : itemname,
+            IsControlDown : IsControlDown,
         })
+    }
+}
+
+function UpdateCooldowns(){
+    $.Schedule(1.1, UpdateCooldowns); 
+    const PlayerID = Players.GetLocalPlayer()
+    const playerIndex = Players.GetPlayerHeroEntityIndex( PlayerID )
+    for(let itemname of ITEMS_TO_VIEW){
+        const itemPanel = $(`#${itemname}_Panel`)
+        itemPanel.FindChildTraverse("ItemCD").visible = false
+    }
+    for(let buffIndex = 0; buffIndex < Entities.GetNumBuffs( playerIndex ); buffIndex++){
+        const buff = Entities.GetBuff( playerIndex, buffIndex )
+        const buffName = Buffs.GetName( playerIndex, buff )
+        for(let itemname of ITEMS_TO_VIEW){
+            const modifierCdName = `modifier_${itemname}_cd`
+            const itemPanel = $(`#${itemname}_Panel`)
+            if(buffName == modifierCdName){
+                const RemainingTime = Buffs.GetRemainingTime( playerIndex, buff )
+                itemPanel.FindChildTraverse("ItemCD").text = Math.floor(RemainingTime)
+                itemPanel.FindChildTraverse("ItemCD").visible = true
+            }
+        }
     }
 }
 
 (function(){
     GameEvents.Subscribe( "initShop", InitStash)
+    GameEvents.Subscribe( "SetShopItemCount", (t)=>{
+        const itemname = t.itemname
+        if(!itemname || !ITEMS_TO_VIEW.includes(itemname)) return
+        const categoryKey = t.categoryKey
+        const itemKey = t.itemKey
+        const count = t.count
+        const itemPanel = $(`#${itemname}_Panel`)
+        itemPanel.FindChildTraverse("ItemCount").text = count
+        itemPanel.visible = true
+    })
+    UpdateCooldowns()
 })();
